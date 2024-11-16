9923b276b456d530247131dccf611ce4b9fc2d08
V-M-B <vmbvarun@gmail.com>
2024-11-16 12:44:29 +0530

    VirtualR

--- VirtualR/src/App.jsx
+++ VirtualR/src/App.jsx
@@ -2,6 +2,11 @@
 import React from 'react'
 import { Navbar } from './components/Navbar'
 import { HeroSection } from './components/HeroSection'
+import { FeatureSection } from './components/FeatureSection'
+import WorkFlow from './components/WorkFlow'
+import { Pricing } from './components/Pricing'
+import { Testimonials } from './components/Testimonials'
+import { Footer } from './components/Footer'
 
 export default function App(props) {
   
@@ -9,9 +14,15 @@ export default function App(props) {
   return (
     <>
       <Navbar/>
-      <div className="max-7-xl mx-auto pt-20 px6">
+      <div className="max-7-xl mx-auto pt-20 px-6">
       <HeroSection/>
       </div>
+      <FeatureSection/>
+      <WorkFlow/>
+      <Pricing/>
+      <Testimonials/>
+      <Footer/>
+      
     </>
   )
 }
